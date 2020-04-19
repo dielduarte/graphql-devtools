0bc1391b50f8f7986213d7fd4daacdd232de3be3
Diel Duarte <magdiel.kesller@gmail.com>
2020-04-19 16:12:04 -0300

    chore: add header content prop

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,13 +4,15 @@ import styles from './Header.module.css';
 interface HeaderProps {
   title: string;
   Icon: React.ReactElement;
+  Content: React.ReactElement;
 }
 
-function Header({ title, Icon }: HeaderProps) {
+function Header({ title, Icon, Content }: HeaderProps) {
   return (
     <header className={styles.header}>
       {Icon}
       <h1 className={styles.title}>{title}</h1>
+      {Content}
     </header>
   );
 }
