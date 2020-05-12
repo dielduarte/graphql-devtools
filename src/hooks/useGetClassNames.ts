eabb916f385dd9574b9ca4aed98cb5ce1f00701d
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-12 23:40:04 +0200

    feat(ClassName): remove console.log

--- src/hooks/useGetClassNames.ts
+++ src/hooks/useGetClassNames.ts
@@ -4,14 +4,6 @@ import React from 'react';
 export default <T>(styles: Record<string, string>) => {
   const [internalStyles] = React.useState(styles);
   return (conditions: { [key in keyof T]?: boolean }): string => {
-    console.log(
-      'From Inside: ',
-      classNames(
-        Object.entries(conditions).reduce((acc, [key, value]) => {
-          return { ...acc, [internalStyles[key]]: value ?? false };
-        }, {}),
-      ),
-    );
     return classNames(
       Object.entries(conditions).reduce((acc, [key, value]) => {
         return internalStyles[key] ? { ...acc, [internalStyles[key]]: value ?? false } : acc;
